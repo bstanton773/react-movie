bb2408764effe1bfc41a371131be8258d0c965e9
Brian <bstanton773@gmail.com>
2021-12-08 23:41:19 -0600

    Fix the movie cards

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,9 +12,11 @@ export default class Home extends Component {
     render() {
         return (
             <div>
+                {/* Search Bar */}
                 <Row>
                     <SearchBar search={this.props.search} handleSearch={this.props.handleSearch} />
                 </Row>
+                {/* Open Filters and Sort By */}
                 <Row>
                     <Col>
                         <Button className='' variant="primary" onClick={() => this.props.handleFilter(!this.props.displayFilters)}>
@@ -25,13 +27,16 @@ export default class Home extends Component {
                         <SortBy handleSort={this.props.handleSort} />
                     </Col>
                 </Row>
+                {/* Filters */}
                 <Row>
                     {this.props.displayFilters ? <Filters providers={this.props.providers} selectedProviders={this.props.selectedProviders} handleProviderChange={this.props.handleProviderChange} genres={this.props.genres} selectedGenres={this.props.selectedGenres} handleGenreChange={this.props.handleGenreChange} handleYearChange={this.props.handleYearChange} minYear={this.props.minYear} maxYear={this.props.maxYear} /> : null}
                 </Row>
+                {/* Page Buttons */}
                 <div className='d-flex justify-content-between'>
                     {this.props.currentPage > 1 ? <Button variant="primary" onClick={() => this.props.handlePageChange(this.props.currentPage-1)}>Last</Button> : <Button variant="primary" disabled>Last</Button>}
                     {this.props.movies.length === 30 ? <Button variant="primary" onClick={() => this.props.handlePageChange(this.props.currentPage+1)}>Next</Button> : <Button variant="primary" disabled>Next</Button>}
                 </div>
+                {/* Movie Gallery */}
                 <Row>
                 {this.props.movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
                 </Row>
