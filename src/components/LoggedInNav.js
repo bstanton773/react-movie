500b0b4f2a30c73d60b830d5ff4acccb4da9bac5
Brian <bstanton773@gmail.com>
2022-02-16 17:13:15 -0600

    Updated navbar to collapse on click

--- src/components/LoggedInNav.js
+++ src/components/LoggedInNav.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 export default function LoggedInNav(props) {
     return (
         <>
-        <Nav.Link as={Link} to='/watchlist'>My Watchlist</Nav.Link>
-        <Nav.Link as={Link} to='/my-ratings'>My Ratings</Nav.Link>
-        <Nav.Link as={Link} to='/following'>Following</Nav.Link>
-        <Nav.Link as={Link} to='/' onClick={props.logout}>Logout</Nav.Link>   
+        <Nav.Link as={Link} to='/watchlist' href='/'>My Watchlist</Nav.Link>
+        <Nav.Link as={Link} to='/my-ratings' href='/'>My Ratings</Nav.Link>
+        <Nav.Link as={Link} to='/following' href='/'>Following</Nav.Link>
+        <Nav.Link as={Link} to='/' href='/' onClick={props.logout}>Logout</Nav.Link>
         </>
     );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,14 +8,14 @@ import LoggedOutNav from './LoggedOutNav';
 export default class Navigation extends Component {
     render() {
         return (
-            <Navbar bg="light" variant="light" expand="lg" className='px-3' fixed='top'>
+            <Navbar collapseOnSelect bg="light" variant="light" expand="lg" className='px-3' fixed='top'>
                 <a className="navbar-brand" href="/">
                     My Movie List
                 </a>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/' href='/'>Home</Nav.Link>
                         {this.props.isAuthenticated ? (
                             <LoggedInNav logout={this.props.logout} />
                         ) : (
